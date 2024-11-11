fdab3a338ce1df86f4008ce63cbc6653e4f99ea3
HanjuLee00 <hjulee@knox.edu>
2024-11-10 20:33:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,24 @@ function showPlace(place) {
                             "<p>Jeonju Hanok Village is a traditional Korean village with over 800 traditional hanok houses.</p>" +
                             "<a href='https://en.wikipedia.org/wiki/Jeonju_Hanok_Village' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'hangang') {
-        content.innerHTML = "<h2>Hangang River, Seoul</h2>" +
+        content.innerHTML = "<h2>Hangang River</h2>" +
                             "<img src='images/hangang_river.jpg' alt='Hangang River'>" +
-                            "<p>The Hangang River flows through Seoul, offering beautiful views and plenty of recreational activities.</p>" +
+                            "<p>The Hangang River is a major river in South Korea, flowing through Seoul.</p>" +
                             "<a href='https://en.wikipedia.org/wiki/Han_River_(Korea)' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'baeknokdam') {
-        content.innerHTML = "<h2>Baeknokdam, Jeju Island</h2>" +
+        content.innerHTML = "<h2>Baeknokdam</h2>" +
                             "<img src='images/baeknokdam_jeju.jpg' alt='Baeknokdam'>" +
-                            "<p>Baeknokdam is a volcanic crater lake located at the peak of Hallasan Mountain on Jeju Island.</p>" +
-                            "<a href='https://en.wikipedia.org/wiki/Hallasan#Baengnokdam' target='_blank'>Learn more on Wikipedia</a>";
+                            "<p>Baeknokdam is a crater lake at the top of Hallasan Mountain in Jeju Island.</p>" +
+                            "<a href='https://en.wikipedia.org/wiki/Baengnokdam' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'namsan') {
-        content.innerHTML = "<h2>Namsan, Seoul</h2>" +
+        content.innerHTML = "<h2>Namsan</h2>" +
                             "<img src='images/namsan_seoul.jpg' alt='Namsan'>" +
-                            "<p>Namsan is a popular mountain in the center of Seoul, home to the iconic N Seoul Tower.</p>" +
-                            "<a href='https://en.wikipedia.org/wiki/N_Seoul_Tower' target='_blank'>Learn more on Wikipedia</a>";
+                            "<p>Namsan is a mountain in the center of Seoul with a popular tower and scenic views.</p>" +
+                            "<a href='https://en.wikipedia.org/wiki/Namsan_(Seoul)' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'hongdae') {
-        content.innerHTML = "<h2>Hongdae, Seoul</h2>" +
+        content.innerHTML = "<h2>Hongdae</h2>" +
                             "<img src='images/hongdae_seoul.jpg' alt='Hongdae'>" +
-                            "<p>Hongdae is a vibrant area known for its youthful energy, arts, and indie music scene.</p>" +
+                            "<p>Hongdae is a vibrant district in Seoul known for its youthful and artistic atmosphere.</p>" +
                             "<a href='https://en.wikipedia.org/wiki/Hongdae' target='_blank'>Learn more on Wikipedia</a>";
-    } else {
-        content.innerHTML = "<p>Select a place to see beautiful images and learn more about it.</p>";
     }
 }
