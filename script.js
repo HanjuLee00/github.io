f61b93a153f8ca4d3b8792bcb56454f8dcc0bd89
HanjuLee00 <hjulee@knox.edu>
2024-11-10 19:12:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,40 +3,30 @@ function showPlace(place) {
 
     if (place === 'jeonju') {
         content.innerHTML = "<h2>Jeonju Hanok Village</h2>" +
-                            "<p>Jeonju Hanok Village is a traditional Korean village with over 800 traditional Korean hanok houses.</p>" +
-                            "<h3>Things to Do:</h3>" +
-                            "<ul><li>Wear a hanbok (Korean traditional dress)</li>" +
-                            "<li>Try Jeonju bibimbap</li>" +
-                            "<li>Explore the traditional tea houses</li></ul>";
+                            "<img src='https://upload.wikimedia.org/wikipedia/commons/4/4e/Jeonju_Hanok_Village.jpg' alt='Jeonju Hanok Village'>" +
+                            "<p>Jeonju Hanok Village is a traditional Korean village with over 800 traditional hanok houses.</p>" +
+                            "<a href='https://en.wikipedia.org/wiki/Jeonju_Hanok_Village' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'hangang') {
         content.innerHTML = "<h2>Hangang River, Seoul</h2>" +
+                            "<img src='https://upload.wikimedia.org/wikipedia/commons/e/eb/Hangang_River_Park.jpg' alt='Hangang River'>" +
                             "<p>The Hangang River flows through Seoul, offering beautiful views and plenty of recreational activities.</p>" +
-                            "<h3>Things to Do:</h3>" +
-                            "<ul><li>Rent a bike and ride along the river</li>" +
-                            "<li>Enjoy a riverside picnic</li>" +
-                            "<li>Take a night cruise on the river</li></ul>";
+                            "<a href='https://en.wikipedia.org/wiki/Han_River_(Korea)' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'baeknokdam') {
         content.innerHTML = "<h2>Baeknokdam, Jeju Island</h2>" +
+                            "<img src='https://upload.wikimedia.org/wikipedia/commons/5/5c/Baekrokdam_Jeju.jpg' alt='Baeknokdam'>" +
                             "<p>Baeknokdam is a volcanic crater lake located at the peak of Hallasan Mountain on Jeju Island.</p>" +
-                            "<h3>Things to Do:</h3>" +
-                            "<ul><li>Hike up Hallasan Mountain</li>" +
-                            "<li>Enjoy the scenic views of the crater lake</li>" +
-                            "<li>Experience the unique flora and fauna of Jeju Island</li></ul>";
+                            "<a href='https://en.wikipedia.org/wiki/Hallasan#Baengnokdam' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'namsan') {
         content.innerHTML = "<h2>Namsan, Seoul</h2>" +
+                            "<img src='https://upload.wikimedia.org/wikipedia/commons/9/9e/Namsan_Seoul.jpg' alt='Namsan'>" +
                             "<p>Namsan is a popular mountain in the center of Seoul, home to the iconic N Seoul Tower.</p>" +
-                            "<h3>Things to Do:</h3>" +
-                            "<ul><li>Visit N Seoul Tower and enjoy panoramic views of Seoul</li>" +
-                            "<li>Take a leisurely walk up the mountain</li>" +
-                            "<li>See the famous love locks at N Seoul Tower</li></ul>";
+                            "<a href='https://en.wikipedia.org/wiki/N_Seoul_Tower' target='_blank'>Learn more on Wikipedia</a>";
     } else if (place === 'hongdae') {
         content.innerHTML = "<h2>Hongdae, Seoul</h2>" +
+                            "<img src='https://upload.wikimedia.org/wikipedia/commons/1/19/Hongdae_Street_Seoul.jpg' alt='Hongdae'>" +
                             "<p>Hongdae is a vibrant area known for its youthful energy, arts, and indie music scene.</p>" +
-                            "<h3>Things to Do:</h3>" +
-                            "<ul><li>Explore street art and mural alleys</li>" +
-                            "<li>Visit unique cafes and restaurants</li>" +
-                            "<li>Enjoy live performances and street busking</li></ul>";
+                            "<a href='https://en.wikipedia.org/wiki/Hongdae' target='_blank'>Learn more on Wikipedia</a>";
     } else {
-        content.innerHTML = "<p>Select a tourist spot to see more information here.</p>";
+        content.innerHTML = "<p>Select a place to see beautiful images and learn more about it.</p>";
     }
 }
